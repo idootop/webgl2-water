84ab3cd206181ba1b324bc442fa20cd26f7867f3
Del Wang <wjg12341234@163.com>
2025-12-14 13:01:38 +0800

    chore: disable https

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 
 export default defineConfig({
   base: "/webgl2-water/",
   server: {
     host: true,
-    https: {
-      // mkcert example.local "192.168.31.125" localhost 127.0.0.1 ::1
-      cert: fs.readFileSync(path.resolve(__dirname, "temp/cert.pem")),
-      key: fs.readFileSync(path.resolve(__dirname, "temp/key.pem")),
-    },
+    // https: {
+    //   // mkcert example.local "192.168.31.125" localhost 127.0.0.1 ::1
+    //   cert: fs.readFileSync(path.resolve(__dirname, "temp/cert.pem")),
+    //   key: fs.readFileSync(path.resolve(__dirname, "temp/key.pem")),
+    // },
   },
   build: {
     outDir: "dist",
